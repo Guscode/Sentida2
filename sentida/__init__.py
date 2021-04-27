71e8d04315ee497973fc1ad3a9ddc79fdc2ed8e0
jacdals97 <54765352+jacdals97@users.noreply.github.com>
2021-04-27 10:44:31 +0200

    Added spaCy model

--- sentida/__init__.py
+++ sentida/__init__.py
@@ -1,2 +1,10 @@
 from .vaderSentiment_da import SentimentIntensityAnalyzer
 from .Model import PolarityModel
+import spacy
+try:
+    nlp = spacy.load('da_core_news_lg')
+except OSError:
+    print('Downloading language model for the spaCy POS tagger\n'
+        "(don't worry, this will only happen once)", file=stderr)
+    from spacy.cli import download
+    download('da_core_news_lg')
